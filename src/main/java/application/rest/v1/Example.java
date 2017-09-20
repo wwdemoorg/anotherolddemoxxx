d9e5b7df427f214163276d70dd20bca6b2d705ab
wwdemo <cbjohann@us.ibm.com>
2017-09-20 10:50:22 -0500

    V2 of Example.java

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -1,19 +1,11 @@
 package application.rest.v1;
-
 import java.util.List;
-
 import javax.ws.rs.GET;
-
 import javax.ws.rs.Path;
-
 import javax.ws.rs.Produces;
-
 import javax.ws.rs.core.MediaType;
-
 import javax.ws.rs.core.Response;
-
 import javax.ws.rs.core.Response.Status;
-
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.logging.*;
@@ -23,7 +15,7 @@ import java.util.logging.*;
 public class Example {
  @GET
  public String greet() throws Exception {
-   return "Hello from " + java.net.InetAddress.getLocalHost().getHostName();
+   return "Hello V2 from " + java.net.InetAddress.getLocalHost().getHostName();
  }
 }
 
